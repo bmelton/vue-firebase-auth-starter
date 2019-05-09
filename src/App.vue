1989ad2a1ada13d6caec0ca641e422d73438de7b
Barry Melton <barry.melton@gmail.com>
2019-05-09 13:26:17 -0400

    Added dummy link to dashboard to test protected routes.

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> | 
+      <router-link to="/dashboard">Dashboard</router-link> | 
       <router-link to="/login">Log in</router-link> | 
       <router-link to="/logout">Log out</router-link>
     </div>
