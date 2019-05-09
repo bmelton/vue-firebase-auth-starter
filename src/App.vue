4fc353a9c73d5c14445825464869c3047d12cf76
Barry Melton <barry.melton@gmail.com>
2019-05-09 12:45:52 -0400

    Added log out link. lol

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,8 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> | 
-      <router-link to="/login">Login</router-link>
+      <router-link to="/login">Log in</router-link> | 
+      <router-link to="/logout">Log out</router-link>
     </div>
     <router-view/>
   </div>
