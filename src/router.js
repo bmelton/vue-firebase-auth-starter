27436206422a25d5f89af44980c0708482d7be6c
Barry Melton <barry.melton@gmail.com>
2019-05-09 12:45:36 -0400

    Protected routes, but it's janky. Might move to plugin that wraps router.

--- src/router.js
+++ src/router.js
@@ -1,25 +1,25 @@
+import firebase from 'firebase';
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import Login from './views/Login.vue'
+import Logout from './views/Logout.vue'
 import Register from './views/Register.vue'
 import Dashboard from './views/Dashboard.vue'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/dashboard',
-      name: 'dashboard',
-      component: Dashboard
+    { path: '*', redirect: '/login' },
+    { path: '/', name: 'home', component: Home },
+    { 
+      path: '/dashboard', 
+      name: 'dashboard', 
+      component: Dashboard,
+      meta: { requiresAuth: true }
     },
     {
       path: '/about',
@@ -29,15 +29,19 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
     },
-    { 
-      path: '/login',
-      name: 'Login',
-      component: Login
-    },
-    { 
-      path: '/register',
-      name: 'Register',
-      component: Register
-    }
+    { path: '/login', name: 'Login', component: Login },
+    { path: '/logout', name: 'Logout', component: Logout },
+    { path: '/register', name: 'Register', component: Register }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  const currentUser = firebase.auth().currentUser;
+  const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
+
+  console.log(currentUser);
+  console.log(requiresAuth);
+  next();
+});
+
+export default router;
