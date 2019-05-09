a53959b33ed4f97615ddb906836416bf5d74d09c
Barry Melton <barry.melton@gmail.com>
2019-05-09 12:44:07 -0400

    Implemented dirty logout view.

---
+++ src/views/Logout.vue
@@ -0,0 +1,22 @@
+<template>
+</template>
+
+<script>
+    import firebase from 'firebase';
+
+    export default {
+        name: "logout",
+        created: function() {
+            firebase.auth().signOut().then(() => {
+                this.$router.replace('login');
+            })
+        },
+        methods: {
+            logout: function() {
+                firebase.auth().signOut().then(() => {
+                    this.$router.replace('login');
+                })
+            }
+        }
+    }
+</script>
