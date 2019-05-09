79e89a960fb87a0e29a5283188ae54c31a9ad7af
Barry Melton <barry.melton@gmail.com>
2019-05-09 10:56:52 -0400

    Created a stub registration view.

---
+++ src/views/Register.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="register">
+        <p>Sign up for a new account</p>
+        <input type="text" placeholder="email" /><br />
+        <input type="password" placeholder="Password" /><br />
+        <button>Sign Up</button>
+        <p>
+            <router-link to="/login">Log in</router-link> instead.
+        </p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'register',
+        data() {
+            return {}
+        },
+        methods: {}
+    }
+</script>
+
+<style scoped>
+</style>
