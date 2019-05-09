033ddbe693c45cb66ea893795d8e5b06242c7a2e
Barry Melton <barry.melton@gmail.com>
2019-05-09 10:48:25 -0400

    Adding Login view.

---
+++ src/views/Login.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="login">
+        <h3>Sign In</h3>
+        <input type="text" placeholder="email" /><br />
+        <input type="password" placeholder="password" /><br />
+        <button>Log In</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'login',
+        data() {
+            return {};
+        },
+        methods: {}
+    }
+</script>
+
+<style scoped>
+    .login {
+        margin-top: 40px;
+    }
+
+    input {
+        margin: 10px 0;
+        width: 10%;
+        cursor: pointer;
+    }
+
+    p {
+        margin-top: 40px;
+        font-size: 13px;
+    }
+
+    p a { 
+        text-decoration: underline;
+        cursor: pointer;
+    }
+</style>
